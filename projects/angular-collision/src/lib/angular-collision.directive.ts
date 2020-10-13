fd899727aff5b354e5453fe4969080838132d93b
kaziupir <kaziupir@gmail.com>
2020-10-13 22:39:15 +0200

    Add animations, display coordinates

--- projects/angular-collision/src/lib/angular-collision.directive.ts
+++ projects/angular-collision/src/lib/angular-collision.directive.ts
@@ -1,10 +1,47 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  AfterContentChecked,
+  ElementRef,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+  Input,
+} from '@angular/core';
+import { timer, Subscription } from 'rxjs';
 
 @Directive({
-  selector: '[ngcAngularCollision]'
+  selector: '[ngcAngularCollision]',
 })
-export class AngularCollisionDirective {
+export class AngularCollisionDirective
+  implements OnInit, AfterContentChecked, OnDestroy {
+  @Input() public disableInterval: boolean; // TODO add to config
+  @Input() public intervalTime: number = 100; // TODO add to config
+  @Output() public rectChange: EventEmitter<DOMRect> = new EventEmitter();
 
-  constructor() { }
+  private timerSubscription: Subscription;
 
+  constructor(private element: ElementRef) {}
+
+  public ngOnInit(): void {
+    if (!this.disableInterval) {
+      this.timerSubscription = timer(0, this.intervalTime).subscribe(() => {
+        this.checkPosition();
+      });
+    }
+  }
+
+  public ngAfterContentChecked(): void {
+    this.checkPosition();
+  }
+
+  public ngOnDestroy(): void {
+    if (!this.timerSubscription.closed) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  public checkPosition(): void {
+    this.rectChange.emit(this.element.nativeElement.getBoundingClientRect());
+  }
 }
--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface Rectangle {
+  name: RectanglesEnum;
+  position: DOMRect;
+}
+
+enum RectanglesEnum {
+  ONE = 'one',
+  TWO = 'two',
+  THREE = 'three',
+}
+
 @Component({
   selector: 'ngc-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo-app';
+  public rectangles: Rectangle[] = [
+    {
+      name: RectanglesEnum.ONE,
+      position: null,
+    },
+    {
+      name: RectanglesEnum.TWO,
+      position: null,
+    },
+    {
+      name: RectanglesEnum.THREE,
+      position: null,
+    },
+  ];
+
+  public handleRectChange(
+    position: DOMRect,
+    rectangleName: RectanglesEnum
+  ): void {
+    this.rectangles.find(
+      (rectangle) => rectangle.name === rectangleName
+    ).position = position;
+    console.log(this.rectangles);
+  }
 }
