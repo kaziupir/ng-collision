bcbd800d325b4c0a8ed2ce58ad7ed86bf6e7f88e
kaziupir <kaziupir@gmail.com>
2020-10-17 18:52:54 +0200

    Save tracked elements as map

--- projects/angular-collision/src/lib/angular-collision.service.ts
+++ projects/angular-collision/src/lib/angular-collision.service.ts
@@ -1,18 +1,54 @@
-import { Injectable, Optional, QueryList } from '@angular/core';
-import { AngularCollisionDirective } from './angular-collision.directive';
+import { Injectable, OnDestroy, Optional, QueryList } from '@angular/core';
+import { takeUntil, throttleTime } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
+import {
+  AngularCollisionDirective,
+  NgcElementChange,
+} from './angular-collision.directive';
 
 export class NgcConfig {}
+
+export interface NgcTrackedElement {
+  id: number;
+  subscription: Subscription;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class AngularCollisionService {
+export class AngularCollisionService implements OnDestroy {
   /**
    *  Tracked elements ids
    */
-  public trackedElements: number[] = [];
+  public trackedElements: NgcTrackedElement[] = [];
 
-  constructor(@Optional() config?: NgcConfig) {}
+  /**
+   *  Map of tracked elements positions
+   */
+  public trackedElementsPositions: Map<number, NgcElementChange> = new Map();
+
+  /**
+   *  Observable wrapper of tracked elements positions
+   */
+  public trackedElementsPositions$: Subject<
+    Map<number, NgcElementChange>
+  > = new Subject();
+
+  public trackedElementsPositionSubscription: Subscription;
+
+  constructor(@Optional() config?: NgcConfig) {
+    this.trackedElementsPositionSubscription = this.trackedElementsPositions$
+      .pipe(throttleTime(100))
+      .subscribe((elementsMap: Map<number, NgcElementChange>) => {
+        elementsMap.forEach((element: NgcElementChange, id: number) => {});
+      });
+  }
+
+  public ngOnDestroy(): void {
+    if (!this.trackedElementsPositionSubscription.closed) {
+      this.trackedElementsPositionSubscription.unsubscribe();
+    }
+  }
 
   /**
    * Start tracking element / elements
@@ -54,17 +90,45 @@ export class AngularCollisionService {
     }
   }
 
+  /**
+   * Init tracking of element
+   *
+   * @param element
+   */
   private addTrackedElement(element: AngularCollisionDirective): void {
-    if (!this.trackedElements.includes(element.id)) {
+    if (!this.trackedElements.some((el) => el.id == element.id)) {
       element.startTracking();
-      this.trackedElements.push(element.id);
+      const subscription = element.rectangleChange$
+        .pipe(takeUntil(element.destroy$))
+        .subscribe((change: NgcElementChange) => {
+          this.trackedElementsPositions.set(change.element.id, change);
+          this.trackedElementsPositions$.next(this.trackedElementsPositions);
+        });
+      this.trackedElements.push({ id: element.id, subscription });
     }
   }
 
+  /**
+   * Finish tracking of element
+   *
+   * @param element
+   */
   private removeTrackedElement(element: AngularCollisionDirective): void {
     element.stopTracking();
+
+    this.trackedElementsPositions.delete(element.id);
+    this.trackedElementsPositions$.next(this.trackedElementsPositions);
+
+    const foundElement = this.trackedElements.find(
+      (trackedElement) => trackedElement.id === element.id
+    );
+
+    if (foundElement?.subscription && !foundElement.subscription.closed) {
+      foundElement.subscription.unsubscribe();
+    }
+
     this.trackedElements = this.trackedElements.filter(
-      (trackedElement) => trackedElement !== element.id
+      (trackedElement) => trackedElement.id !== element.id
     );
   }
 }
